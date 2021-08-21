361c3b8560bed6dc62eab27d36f344d6416752d2
AhmadHamdy1101 <32060651+AhmadHamdy1101@users.noreply.github.com>
2021-08-21 12:00:32 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var hambrger = document.querySelector(".humperger");
+var menu = document.querySelector(".main-menu");
+var closemenu = document.querySelector(".close");
+
+function hmenu() {
+  menu.style.display="block";
+  closemenu.style.display="block";
+  closemenu.style.animation="closeanim 1s"
+  
+}
+hambrger.addEventListener("click", hmenu);
+closemenu.addEventListener("click",function () {
+  menu.style.display="none";
+  closemenu.style.display="none";
+});
+
+const swiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 5000,
+      },
+ 
+    speed: 400,
+    loop: true,}
+    );
+
+    $("body").niceScroll({
+      cursorcolor: "#3a1984",
+      cursorborder: "none",
+      scrollspeed: 50,
+    });
